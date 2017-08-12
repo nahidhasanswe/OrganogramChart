bc6e405b9a1bc69c9293c64d8de8cd02fde58133
Nahid Hasan <nahid675@diu.edu.bd>
2017-08-12 12:29:59 +0600

    Add project files.

---
+++ OrganogramChart/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using OrganogramChart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrganogramChart.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public JsonResult getChartData()
+        {
+            List<EmployeeTable> list = new List<EmployeeTable>();
+            using (dbEntities dc = new dbEntities())
+            {
+                list = dc.EmployeeTables.ToList();
+            }
+
+            return new JsonResult { Data = list, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+    }
+}
---
+++ OrganogramChart/Scripts/myApp.js
@@ -0,0 +1,44 @@
+﻿var app = angular.module('myApp', []);
+app.controller('myController', ['$scope', '$http', function ($scope, $http) {
+    //here http get method for get data from database
+
+    $scope.View = function () { alert('ghd');}
+
+    $scope.chartData = [['Name', 'ReportsTo', 'tooltip']];
+    $http.get('/home/getChartData').then(function (response) {
+        var newobject = [['Name', 'ReportsTo', 'tooltip']];
+        angular.forEach(response.data, function (val) {
+            newobject.push(
+                [
+                    {
+                        v: val.EmployeeID.toString(),
+                        f: '<div class="customBox" onClick="View()"><div>' + (val.FirstName + ' ' + val.LastName) + '</div><div class="title">' + val.Title + '</div><div class="title">' + val.TitleOfCourtesy + '</div></div>'
+                    },
+                    (val.ReportsTo.toString() == "0" ? "" : val.ReportsTo.toString()),
+                    (val.FirstName + ' ' + val.LastName)
+                ]
+                );
+
+        })
+        $scope.chartData = newobject;
+    })
+}])
+app.directive('orgChart', function () {
+    function link($scope, element, attrs) {
+        var chart = new google.visualization.OrgChart(element[0]);
+        $scope.$watch('chartData', function (value, oldvalue) {
+            if (!value) {
+                return;
+            }
+            var data = google.visualization.arrayToDataTable(value);
+            var options = {
+                'title': '',
+                'allowHtml': true
+            }
+            chart.draw(data, options);
+        })
+    }
+    return {
+        link: link
+    };
+})
